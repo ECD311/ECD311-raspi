03b949cd757de85c8a998ce689d536d0c189c90b
Bea Mulvey <22732775+bmulvey1@users.noreply.github.com>
2023-04-19 12:27:12 -0400

    use fewer wildcards for filenames, more explicitly specify file to open

--- cleanup_archive.py
+++ cleanup_archive.py
@@ -27,16 +27,17 @@ for file in glob.glob(src+"*.csv"):
 for file in os.listdir(src):
     if os.path.isdir(src+file):
         year = file.split("/")[-1]
+        print(year)
         for month in range(1,13):
             for day in range(1,32):
-                files = os.path.join(src, file, str(month), "data_log_*_*_%02i_*.csv" % day)
+                files = os.path.join(src, file, "%02i" % month, "data_log_%s_%02i_%02i_*.csv" % (year, month, day))
                 files = glob.glob(files)
                 try:
                     combined_csv = pd.concat([pd.read_csv(f) for f in files])
                 except:
                     continue
-                combined_csv.to_csv(os.path.join(src, str(year), str(month), "data_log_%s_%02i_%02i.csv" % (year, month, day)))
-                print(os.path.join(src, str(year), str(month), "data_log_%s_%02i_%02i.csv" % (year, month, day)))
+                combined_csv.to_csv(os.path.join(src, str(year), "%02i" % month, "data_log_%s_%02i_%02i.csv" % (year, month, day)), index=False)
+                print(os.path.join(src, str(year), "%02i" % month, "data_log_%s_%02i_%02i.csv" % (year, month, day)))
 
 # remove existing files after concat
 
